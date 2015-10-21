f9d1677406e022b8d9f111c911955c6487bc3edc
Maurits van der Schee <maurits@vdschee.nl>
2015-10-21 03:16:27 +0200

    Initial commit

---
+++ tail.php
@@ -0,0 +1,17 @@
+<?php
+$file = '/var/log/apache2/access.log';
+$title = "\e[H\e[2K$file";
+echo "\e[m\e[2J";
+$stream = fopen($file, 'r');
+fseek($stream, 0, SEEK_END);
+echo str_repeat("\n",4500)."\e[s$title";
+flush();
+while(true){
+  $data = stream_get_contents($stream,-1);
+  if ($data) {
+    echo "\e[32m\e[u".$data."\e[s".str_repeat("\e[m",1500)."$title";
+    flush();
+  }
+  usleep(100000);
+}
+fclose($stream);
