0e88424b8712e0e059061c53a0f0ae13810724c4
Maurits van der Schee <maurits@vdschee.nl>
2015-10-21 03:41:51 +0200

    Added security

--- tail.php
+++ tail.php
@@ -1,8 +1,10 @@
 <?php
 $file = '/var/log/apache2/access.log';
 $title = "\e[H\e[2K$file";
-echo "\e[m\e[2J";
+if (strpos($_SERVER['REMOTE_ADDR'],'127.')!==0) die('Access Denied');
 $stream = fopen($file, 'r');
+if (!$stream) die("Could not open file: $file\n");
+echo "\e[m\e[2J";
 fseek($stream, 0, SEEK_END);
 echo str_repeat("\n",4500)."\e[s$title";
 flush();
