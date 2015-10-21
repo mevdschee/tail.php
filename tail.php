b602ddacf6d73e6043b6bebd9ed0160eeabb2dcb
Maurits van der Schee <m.vanderschee@leaseweb.com>
2015-10-21 10:03:50 +0200

    Update tail.php

--- tail.php
+++ tail.php
@@ -3,18 +3,18 @@
 $file = '/var/log/apache2/access.log';
 $ip = '127.';
 // start of script
-$title = "\e[H\e[2K$file";
+$title = "\033[H\033[2K$file";
 if (strpos($_SERVER['REMOTE_ADDR'],$ip)!==0) die('Access Denied');
 $stream = fopen($file, 'r');
 if (!$stream) die("Could not open file: $file\n");
-echo "\e[m\e[2J";
+echo "\033[m\033[2J";
 fseek($stream, 0, SEEK_END);
-echo str_repeat("\n",4500)."\e[s$title";
+echo str_repeat("\n",4500)."\033[s$title";
 flush();
 while(true){
   $data = stream_get_contents($stream);
   if ($data) {
-    echo "\e[32m\e[u".$data."\e[s".str_repeat("\e[m",1500)."$title";
+    echo "\033[32m\033[u".$data."\033[s".str_repeat("\033[m",1500)."$title";
     flush();
   }
   usleep(100000);
